30e33ad3e692503d9ddb23301eb31382851f4187
Peter <peter2213@github.com>
2025-03-26 11:41:38 +0200

    handling the FBV

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from django.http.response import JsonResponse
+from .models import Guest, Reservation, Movie
+from rest_framework.decorators import api_view
+from .serializers import GuestSerializer, MovieSerializer, ReservationSerializer
 
 # Create your views here.
+# Imaginating without rest_framework 
+def no_rest(request):
+    data = Guest.objects.all()
+    res_dict = {
+        'Guest' : list(data.values('guest_name', 'phone_number'))
+    }
+    return JsonResponse(res_dict)
+
+# GET, POST
+@api_view(['GET', 'POST'])
+def FBV_List(request):
+    if request.method == 'GET':
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer =  GuestSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        return Response(serializer.data, status= status.HTTP_400_BAD_REQUEST)  
+        
