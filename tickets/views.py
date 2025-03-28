6b1c9b090be44e83f58fc760dda353165fe30986
Peter <peter2213@github.com>
2025-03-28 15:39:34 +0200

    handling the crud operations

--- tickets/views.py
+++ tickets/views.py
@@ -20,4 +20,23 @@ def FBV_List(request):
             serializer.save()
             return Response(serializer.data, status= status.HTTP_201_CREATED)
         return Response(serializer.data, status= status.HTTP_400_BAD_REQUEST)  
+
+# GET, PUT, DELETE 
+@api_view(['GET', 'PUT', 'DELETE'])
+def FBV_pk(request, pk):
+    guest = Guest.objects.get(pk=pk)
+    if request.method == 'GET':
+        serializer = GuestSerializer(guest, many = True)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = GuestSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_202_ACCEPTED)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        guest.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
         
+    
